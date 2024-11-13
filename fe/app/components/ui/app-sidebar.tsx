df9382d828ce03d8ca10ed76bf93117889d73d0c
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-11-13 13:30:39 +0800

    feat:sidebar

---
+++ fe/app/components/ui/app-sidebar.tsx
@@ -0,0 +1,63 @@
+import { Link } from "@remix-run/react"
+import { ArrowRightLeft, ChartCandlestick, FileChartColumn, Gauge, Settings } from "lucide-react"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+    SidebarMenuButton,
+    SidebarMenuSub,
+} from "~/components/ui/sidebar"
+
+
+export function AppSidebar() {
+    return (
+        <Sidebar>
+            <SidebarHeader>
+                <p className="text-lg font-bold">
+                    Finance Tracker
+
+                </p>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarMenuButton asChild>
+                        <Link to="/">
+                            <Gauge />
+                            Dashboard
+                        </Link>
+                    </SidebarMenuButton>
+                    <SidebarMenuButton asChild>
+                        <Link to="/portfolio">
+                            <ChartCandlestick />
+                            Portfolio
+                        </Link>
+                    </SidebarMenuButton>
+                    <SidebarMenuSub>
+                        <SidebarMenuButton asChild>
+                            <Link to="/portfolio/transactions">
+                                <ArrowRightLeft />
+                                Transactions
+                            </Link>
+                        </SidebarMenuButton>
+                        <SidebarMenuButton asChild>
+                            <Link to="/portfolio/statistics">
+                                <FileChartColumn />
+                                Statistics
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuSub>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenuButton asChild>
+                    <Link to="/settings">
+                        <Settings />
+                        Settings
+                    </Link>
+                </SidebarMenuButton>
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
