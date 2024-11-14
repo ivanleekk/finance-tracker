2703e7f8de613c112e2eaa52b088b16ee3c3249d
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-11-14 15:50:37 +0800

    feat: statistics page

--- fe/app/routes/portfolio.statistics_.tsx
+++ fe/app/routes/portfolio.statistics_.tsx
@@ -1,8 +1,22 @@
+import { Card } from "~/components/ui/card";
+
 export default function Statistics() {
     return (
         <div>
-            <h1>Statistics</h1>
-            <p>View your statistics here</p>
+            <div className="space-y-2">
+                <Card className="w-fit p-2">
+                    <h2>Portfolio Beta</h2>
+                    <p>0.67</p>
+                </Card>
+                <Card className="w-fit p-2">
+                    <h2>Portfolio Standard Deviation</h2>
+                    <p>0.25</p>
+                </Card>
+                <Card className="w-fit p-2">
+                    <h2>Portfolio Sharpe Ratio</h2>
+                    <p>0.75</p>
+                </Card>
+            </div>
         </div>
     );
 }
