b0ee896b31fb63825a32b495907d88960b979298
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-11-20 00:06:44 +0800

    feat: add title

--- fe/app/root.tsx
+++ fe/app/root.tsx
@@ -38,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <head>
+                <title>Finance Tracker</title>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <Meta />
