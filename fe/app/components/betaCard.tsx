ba409fd6e005400bd72b948c1dcac70b2e82ce37
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-11-21 19:08:45 +0800

    fix: beta 4dp

--- fe/app/components/betaCard.tsx
+++ fe/app/components/betaCard.tsx
@@ -18,7 +18,7 @@ export default function BetaCard({ data }) {
     return (
         <Card className="w-fit p-2">
             <h2>Portfolio Beta</h2>
-            <p>{beta}</p>
+            <p>{beta.toFixed(4)}</p>
         </Card>
     );
 }
