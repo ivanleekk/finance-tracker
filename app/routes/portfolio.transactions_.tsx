0b67b9f5e2d07478fc75027aa6b31bc1824d759f
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-11-29 17:41:08 +0800

    feat: suspense for portfolio transactions page

--- app/routes/portfolio.transactions_.tsx
+++ app/routes/portfolio.transactions_.tsx
@@ -1,24 +1,54 @@
-import {LoaderFunctionArgs, redirect} from "@remix-run/node";
-import {getUserSession} from "~/utils/session.server";
+import {LoaderFunctionArgs} from "@remix-run/node";
 import {getTransactions} from "~/portfolio/portfolio";
-import {useLoaderData} from "@remix-run/react";
+import {Await, useLoaderData} from "@remix-run/react";
 import {DataTable} from "~/components/dataTable";
 import {transactionColumns} from "~/portfolio/transactionColumns";
 import {requireUserSession} from "~/utils/auth.server";
+import {Card, CardHeader} from "~/components/ui/card";
+import {Suspense} from "react";
+import {portfolioColumns} from "~/portfolio/portfolioColumns";
+import {defer} from "@vercel/remix";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
     await requireUserSession(request);
 
-
-    return await getTransactions(request);
+    const transactions = getTransactions(request);
+    const result = {
+        "transactionData": transactions,
+    }
+    return defer(result);
 };
 
 export default function Transactions() {
-    const transactionData = useLoaderData();
+    const {transactionData} = useLoaderData<typeof loader>();
 
     return (
-        <div>
-            <DataTable columns={transactionColumns} data={transactionData} />
-        </div>
+        <Card className="w-fit flex-grow basis-auto p-2">
+            <CardHeader className="text-xl font-bold">
+                Transactions
+            </CardHeader>
+            <Suspense
+                fallback={
+                    <DataTable
+                        columns={portfolioColumns}
+                        data={[
+                            {
+                                id: 'abc',
+                                symbol: 'Loading...',
+                                buySell: 'Loading...',
+                                quantity: 0,
+                                price: 0,
+                                totalValue: 0,
+                                date: new Date().toISOString(),
+                            }
+                        ]
+                        }/>}
+            >
+                <Await resolve={transactionData}>
+                    {(transactionData) => <DataTable columns={transactionColumns} data={transactionData}/>}
+
+                </Await>
+            </Suspense>
+        </Card>
     );
 }
