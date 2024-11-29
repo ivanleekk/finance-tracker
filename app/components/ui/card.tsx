4217d2a6ff1ccf0451bdad46db2b1ba2da1d60a2
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-11-29 11:17:03 +0800

    fix: card shadow

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-        "rounded-xl border bg-card text-card-foreground shadow-card overflow-auto",
+        "rounded-xl border bg-card text-card-foreground shadow overflow-auto",
       className
     )}
     {...props}
