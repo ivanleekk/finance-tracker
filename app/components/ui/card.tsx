d4175e11d4c4f83ba0d7c8e11b4bb404acdcef70
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-11-29 11:07:18 +0800

    fix: card borders

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "~/lib/utils"
+import {cn} from "~/lib/utils"
 
 const Card = React.forwardRef<
   HTMLDivElement,
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
+        "rounded-xl border bg-card text-card-foreground shadow-card overflow-auto",
       className
     )}
     {...props}
