06141fbf48c28e39358f2afcd5a951b271ceacd0
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-12-01 15:47:07 +0800

    feat: add redis for banks

--- app/utils/redisKeys.ts
+++ app/utils/redisKeys.ts
@@ -4,4 +4,7 @@ export enum RedisKeys {
     PORTFOLIO = 'portfolio:',
     PORTFOLIO_BETA = 'portfolioBeta:',
     TRANSACTIONS = 'transactions:',
+    BANK = 'bank:',
+    BANK_HISTORY = 'bankHistory:',
+    BANK_HISTORY_MONTHLY = 'bankHistoryMonthly:',
 }
